b9f8db2dfce6391c739cd846ce77c27b4e2158bb
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-06-19 15:19:20 +0530

    Standard Input and Standard Output in NodeJS

---
+++ standard-input-output.js
@@ -0,0 +1,36 @@
+// Example: 01
+process.stdout.write('Hello World\n');
+
+
+
+// Example: 02
+let answers = [];
+let questions = [
+  'What is your name?',
+  'What is your fav hobby?',
+  'What is your preferred programming language?'
+];
+
+function ask(i) {
+  process.stdout.write(`\n ${questions[i]}`);
+  process.stdout.write(' > ');
+}
+
+process.stdin.on('data', function(data) {
+  answers.push(data.toString().trim());
+
+  if (answers.length < questions.length) {
+    ask(answers.length);
+  } else {
+    process.exit();
+  }
+});
+
+ask(0); //function call
+process.on('exit', function() {
+  process.stdout.write('\n\n');
+  process.stdout.write(
+    `Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} later`
+  );
+  process.stdout.write('\n\n');
+});
