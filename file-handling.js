447dce584303ab94a9174bab4d75641dc0e01996
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-06-19 17:05:41 +0530

    File Handling in NodeJS

--- file-handling.js
+++ file-handling.js
@@ -1,1 +1,11 @@
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What is your favorite food? ', answer => {
+  console.log(`Oh!, so your favorite food is ${answer}`);
+  rl.close();
+});
