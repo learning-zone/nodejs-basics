af57b6f6d453c69556068cbb6a3640b55df3b219
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-06-20 10:45:29 +0530

    Exec Module

---
+++ exec.js
@@ -0,0 +1,10 @@
+
+// Example 02: Creating child process with exec() Module
+var exec = require('child_process').exec;
+
+exec('git version', function(err, stdout) {
+  if (err) {
+    throw err;
+  }
+  console.log('\n Git Version function executed. \n', stdout);
+});
