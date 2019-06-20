059bc4a350dca507548793e4f971edffc094c5f5
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-06-20 10:29:48 +0530

    Events in NodeJS

---
+++ events.js
@@ -0,0 +1,42 @@
+// Example: 01 The EventEmitter class is defined and exposed by the events module
+const EventEmitter = require('events'); 
+const util = require('util');
+
+class Event extends EventEmitter {}
+
+const eventObject = new Event();
+eventObject.on('event', () => {
+  console.log('An event occurred!');
+});
+eventObject.emit('event');
+
+
+// Example: 02 Asynchronous vs. Synchronous
+const eventObject2 = new Event();
+eventObject2.on('event', () => {
+  setImmediate(() => {     // Used for Asynchronous function call
+    console.log('Asynchronous function call !');
+  });
+});
+eventObject2.emit('event');
+
+
+// Example: 03 Error Events
+//const eventObject3 = new Event();
+//eventObject3.emit('error', new Error('whoops! An error occurred.'));
+
+
+// Example: 04 
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+util.inherits(Person, EventEmitter);
+var personObject = new Person('Vanesa Doss');
+
+personObject.on('speak', function(said) {
+  console.log(`${this.name} says: ${said}`);
+});
+personObject.emit('speak', 'How are you?');
