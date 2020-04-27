2cfd5575a0418da71743f076b7842d103d43b1a9
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-04-27 12:02:35 +0530

    Global Objects

---
+++ nodejs-basics/03_global_objects.js
@@ -0,0 +1,18 @@
+/**
+ * Global Objects
+ * 
+ */
+
+// Displays current directory
+console.log(__dirname); 
+
+// Displays filename with path
+console.log(__filename); 
+ 
+
+// Displays filename 
+var path = require("path");
+console.log(path.basename(__filename)); 
+
+
+
