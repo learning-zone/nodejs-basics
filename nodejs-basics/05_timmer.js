d37f645bd3d105b042b9941f5720624de5d330a4
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-04-27 16:12:40 +0530

    Timmer in NodeJS

---
+++ nodejs-basics/05_timmer.js
@@ -0,0 +1,28 @@
+/**
+ * NodeJS Timer Module
+ * 
+ */
+
+// Example: setInterval()
+setInterval(function() {  
+   // console.log("setInterval: 1 millisecond completed...");   
+}, 1000);  
+
+
+
+// Example: setTimeout()
+var recursive = function () {  
+    //console.log("Hey! 1000 millisecond completed!..");   
+    setTimeout(recursive, 1000);  
+}  
+recursive();
+
+
+// Example: setInterval(), setTimeout() and clearInterval()
+function welcome () {  
+    console.log("Welcome to JavaTpoint!");  
+}  
+var id1 = setTimeout(welcome, 1010);  
+var id2 = setInterval(welcome, 1000);  
+//clearTimeout(id1);  
+clearInterval(id2);  
