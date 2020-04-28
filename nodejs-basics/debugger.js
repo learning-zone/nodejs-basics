077b5c599d15e5866736d3e28309243e10c8e9ed
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-04-28 16:54:43 +0530

    Debugger Module

---
+++ nodejs-basics/debugger.js
@@ -0,0 +1,17 @@
+/**
+ * Debugger Module
+ * 
+ * Node.js includes an out-of-process debugging utility accessible via a V8 Inspector 
+ * and built-in debugging client. 
+ * 
+ * To use it, start Node.js with the inspect argument followed by the path to the script 
+ * to debug; a prompt will be displayed indicating successful launch of the debugger:
+ * 
+ */
+
+global.x = 5;
+setTimeout(() => {
+  debugger;
+  console.log('World');
+}, 1000);
+console.log('Hello');
