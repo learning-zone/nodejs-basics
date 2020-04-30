3891eff8c87ed3b07b3f0a380008eed2dbbe797f
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-04-30 10:33:25 +0530

    String Decoder

---
+++ nodejs-basics/string-decoder.js
@@ -0,0 +1,18 @@
+/**
+ * String Decoder
+ * 
+ * The Node.js StringDecoder is used to decode buffer into string. 
+ * It is similar to buffer.toString() but provides extra support to UTF.
+ * 
+ */
+
+const { StringDecoder } = require('string_decoder');
+const decoder = new StringDecoder('utf8');
+
+
+const rupee = Buffer.from([0xE2, 0x82, 0xB9]);
+console.log(decoder.write(rupee));
+
+
+const euro = Buffer.from([0xE2, 0x82, 0xAC]);
+console.log(decoder.write(euro));
