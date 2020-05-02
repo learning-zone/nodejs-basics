6fd34b55117b0bda24b07d647489da0a23f2d6ba
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-05-02 16:02:04 +0530

    ALTER Query

---
+++ nodejs-mysql/alter-table.js
@@ -0,0 +1,15 @@
+let db_pool = require('./config').mysql_connect; 
+
+let sql = `ALTER TABLE employee ADD COLUMN salary DOUBLE(10, 2)`; 
+
+// connect to the MySQL server
+db_pool.getConnection((err) => {  
+  if (err) throw err;  
+  console.log("Connected!");  
+ 
+  db_pool.query(sql, (err, result) => {  
+    if (err) throw err;  
+    
+    console.log("Table altered!");  
+  });  
+});  
