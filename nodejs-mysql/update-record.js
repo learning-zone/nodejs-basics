a2305f0bf27b3f390217f04cb3b631e96254a420
Pradeep Kumar <pradeep.vwa@gmail.com>
2020-05-02 16:46:05 +0530

    UPDATE Query

--- nodejs-mysql/update-record.js
+++ nodejs-mysql/update-record.js
@@ -1,14 +1,20 @@
 let db_pool = require('./config').mysql_connect; 
  
-let sql = `UPDATE employee SET city = 'Ranchi' WHERE id = ?`; 
+let sql = `UPDATE employee 
+           SET city = ? 
+           WHERE id = ?`; 
+
+let data = ['Bangalore', 5];
 
 // connect to the MySQL server
 db_pool.getConnection((err) => {  
   if (err) throw err;  
    
-  // execute the SQL QUery
-  db_pool.query(sql, [10], (err, result) => {  
-    if (err) throw err;  
+  // execute the UPDATE statement
+  db_pool.query(sql, data, (error, result) => {  
+    if (error){
+        return console.error(error.message);
+    } 
 
     console.log(result.affectedRows + " record(s) updated");  
   });  
